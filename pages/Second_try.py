2a29528a94bffe65e7151a7a9bb339530012b008
KenichiQaz <31177857+KenichiQaz@users.noreply.github.com>
2022-11-21 08:23:02 +0000

    restart app button

--- pages/Second_try.py
+++ pages/Second_try.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from streamlit_image_comparison import image_comparison
 
+if 'key' not in st.session_state:
+    st.session_state['key'] = 'value'
+
 def show_images(images):
     image_comparison(
         img1=images[0],
@@ -12,10 +15,16 @@ def show_images(images):
 images = []
 
 with st.form("my-form", clear_on_submit=True):
-    images = st.file_uploader('Choose the files you want to compare', type=['png', 'jpg'], accept_multiple_files=True)
+    images = st.file_uploader('Choose the files you want to compare', type=['png', 'jpg'], accept_multiple_files=True, key=1)
     submitted = st.form_submit_button("Compare files")
 
 if submitted is not None:
     show_images(images)
 else:
     st.write('add files')
+
+
+if st.button('Restart the program'):
+    for key in st.session_state.keys():
+        del st.session_state[key]
+    st.experimental_rerun()
