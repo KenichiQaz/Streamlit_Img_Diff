cd99cc50f521aa3b5077c115accc1895bb520cc8
KenichiQaz <31177857+KenichiQaz@users.noreply.github.com>
2022-11-21 07:58:28 +0000

    Create second page

---
+++ pages/Second_try.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from streamlit_image_comparison import image_comparison
+import cv2
+
+#pip install streamlit-cropper
+#pip install streamlit-webrtc
+
+st.header("J. Webb Space Telescope vs Hubble Telescope")
+
+st.write("")
+"This is a reproduction of the fantastic [WebbCompare](https://www.webbcompare.com/index.html) app by [John Christensen](https://twitter.com/JohnnyC1423). It's built in Streamlit and takes only 10 lines of Python code. If you like this app, please star [John's original repo](https://github.com/JohnEdChristensen/WebbCompare)!"
+st.write("")
+images = []
+
+def upload_images():
+    images = st.file_uploader('Choose the files you want to compare', type=['png', 'jpg'], accept_multiple_files=True)
+    show_images(images)
+
+def show_images(images):
+    image_comparison(
+        img1=images[0],
+        img2=images[1],
+        label1="Image 1",
+        label2="Image 2",
+    )
