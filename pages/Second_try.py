28775a6a19eba4c841cdbdd74e3685b87600c7ef
KenichiQaz <31177857+KenichiQaz@users.noreply.github.com>
2022-11-21 08:14:53 +0000

    cleaned files

--- pages/Second_try.py
+++ pages/Second_try.py
@@ -1,11 +1,6 @@
 import streamlit as st
 from streamlit_image_comparison import image_comparison
 
-st.header("J. Webb Space Telescope vs Hubble Telescope")
-
-st.write("")
-"This is a reproduction of the fantastic [WebbCompare](https://www.webbcompare.com/index.html) app by [John Christensen](https://twitter.com/JohnnyC1423). It's built in Streamlit and takes only 10 lines of Python code. If you like this app, please star [John's original repo](https://github.com/JohnEdChristensen/WebbCompare)!"
-st.write("")
 images = []
 
 def upload_images():
