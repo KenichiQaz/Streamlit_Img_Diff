8fb4d8a6b5174e777a9b3f6882d940558cde3115
KenichiQaz <31177857+KenichiQaz@users.noreply.github.com>
2022-11-22 03:53:17 +0000

    Changed whole code base to better execution

--- pages/Second_try.py
+++ pages/Second_try.py
@@ -1,33 +1,32 @@
 import streamlit as st
+from skimage.metrics import structural_similarity
 from streamlit_image_comparison import image_comparison
 import cv2
 import numpy as np
-from PIL import Image
 
 if 'key' not in st.session_state:
     st.session_state['key'] = 'value'
 
-st.set_page_config(page_title="Image-Comparison", layout="centered")
+# set page config
+st.set_page_config(page_title="Image-Comparison Example", layout="centered")
 
-def show_images():
-    image1 = cv2.cvtColor(cv2.imdecode(np.frombuffer(first.read(), np.uint8), 1) , cv2.COLOR_BGR2RGB)
-    image2 = cv2.cvtColor(cv2.imdecode(np.frombuffer(second.read(), np.uint8), 1) , cv2.COLOR_BGR2RGB)
-    image_comparison(
-        img1=image1,
-        img2=image2,
-        label1="Image 1",
-        label2="Image 2",
-    )
 
-with st.form("my-form-2", clear_on_submit=True):
-    first = st.file_uploader("Choose the first file", ['png', 'jpg','jpeg'], key=3)
-    second = st.file_uploader("Choose the second file", ['png', 'jpg','jpeg'], key=4)
+with st.form("my-form", clear_on_submit=True):
+    first_image = st.file_uploader("Choose the first file", ['png', 'jpg'], key=3)
+    second_image = st.file_uploader("Choose the second file", ['png', 'jpg'], key=4)
     submitted = st.form_submit_button("Compare files")
 
-if submitted is not None:
-    show_images()
-else:
-    st.write('add files')
+def img_comparison(before, after):
+    image_comparison(
+        img1=before,
+        img2=after,
+    )
+
+if first_image and second_image and submitted is not None:
+    before = cv2.cvtColor(cv2.imdecode(np.frombuffer(first_image.read(), np.uint8), 1) , cv2.COLOR_BGR2RGB)
+    after = cv2.cvtColor(cv2.imdecode(np.frombuffer(second_image.read(), np.uint8), 1) , cv2.COLOR_BGR2RGB)
+    st.write('Comparing files')
+    img_comparison(before, after)
 
 if st.button('Restart the program'):
     for key in st.session_state.keys():
