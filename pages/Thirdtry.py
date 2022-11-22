6c49590662267aa6a8d5abb338e27e423cccbfaa
KenichiQaz <31177857+KenichiQaz@users.noreply.github.com>
2022-11-22 21:17:37 +0700

    Update Thirdtry.py

--- pages/Thirdtry.py
+++ pages/Thirdtry.py
@@ -14,19 +14,20 @@ if st.button('Restart the program'):
     st.experimental_rerun()
 
 with st.form("my-form3", clear_on_submit=True):
-    images1 = convert_from_bytes(st.file_uploader("Choose the first file", ['pdf'], key=5))
-    images2 = convert_from_bytes(st.file_uploader("Choose the second file", ['pdf'], key=6))
-    submitted = st.form_submit_button("Compare files")
+  files1 = st.file_uploader("Choose the first file", ['pdf'], key=5)
+  files2 = st.file_uploader("Choose the second file", ['pdf'], key=6)
+  submitted = st.form_submit_button("Compare files")
     
 def pdf_comparison():
+    images1 = convert_from_bytes(files1,fmt="png")
+    images2 = convert_from_bytes(files2,fmt="png")
   for index, image in enumerate(images1):
     img1 = images1[index]
     img2 = images2[index]
-    if img1 != img2:
-      compared = compare_images(img1, img2, method='diff')
-      st.image(cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY), caption='First')
-      st.image(cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY), caption='second')
-      st.image(cv2.cvtColor(compared, cv2.COLOR_BGR2GRAY), caption='Diff comparison')
+    compared = compare_images(img1, img2, method='diff')
+    st.image(cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY), caption='First')
+    st.image(cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY), caption='second')
+    st.image(cv2.cvtColor(compared, cv2.COLOR_BGR2GRAY), caption='Diff comparison')
 
 if images1 and images2 and submitted is not None:
     st.write('Comparing files')
