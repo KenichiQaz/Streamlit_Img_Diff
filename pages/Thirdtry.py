6ea9a35d3f34da17f62511ee0923475c95ccd816
KenichiQaz <31177857+KenichiQaz@users.noreply.github.com>
2022-11-22 21:41:47 +0700

    Update Thirdtry.py

--- pages/Thirdtry.py
+++ pages/Thirdtry.py
@@ -23,8 +23,8 @@ def pdf_comparison():
         img1 = images1[index]
         img2 = images2[index]
         compared = compare_images(img1, img2, method='diff')
-        st.image(cv2.cvtColor(cv2.imdecode(img1), cv2.COLOR_BGR2GRAY), caption='First')
-        st.image(cv2.cvtColor(cv2.imdecode(img2), cv2.COLOR_BGR2GRAY), caption='second')
+        st.image(cv2.cvtColor(cv2.imdecode(img1, 1), cv2.COLOR_BGR2GRAY), caption='First')
+        st.image(cv2.cvtColor(cv2.imdecode(img2, 1), cv2.COLOR_BGR2GRAY), caption='second')
         st.image(cv2.cvtColor(cv2.imdecode(compared), cv2.COLOR_BGR2GRAY), caption='Diff comparison')
 
 if files1 and files2 and submitted is not None:
