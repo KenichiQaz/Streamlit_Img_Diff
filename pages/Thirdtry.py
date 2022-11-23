4afd6eb64f756568bb1819dadb5f4376b76c81da
KenichiQaz <31177857+KenichiQaz@users.noreply.github.com>
2022-11-23 14:07:29 +0000

    debug 1

--- pages/Thirdtry.py
+++ pages/Thirdtry.py
@@ -38,12 +38,12 @@ def pdf_comparison():
         img2 = np.array(images2[index])
 
         diff = mse(img1, img2)
-        st.write(diff.type)
+        st.write(str(diff))
 
         #compared = compare_images(img1, img2, method='diff')
-        st.image(cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY), caption='First')
-        st.image(cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY), caption='second')
-        st.image(diff, caption='Diff comparison')
+        #st.image(cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY), caption='First')
+        #st.image(cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY), caption='second')
+        #st.image(diff, caption='Diff comparison')
 
 
 
